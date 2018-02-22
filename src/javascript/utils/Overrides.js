5480f8ba07eb8d658987ff1753ca8b1a3321ca26
Tyr Johanson <tyr@ca.com>
2018-02-22 20:28:32 +0000

    removed unused model decoration

--- src/javascript/utils/Overrides.js
+++ src/javascript/utils/Overrides.js
@@ -1,25 +1,5 @@
 /* global Ext Rally com _ TsSummaryRow */
 Ext.override(Rally.data.wsapi.TreeStore, {
-    /*
-    _decorateModels: function() {
-        var models = this.model;
-
-        if (_.isFunction(models.getArtifactComponentModels)) {
-            models = models.getArtifactComponentModels();
-        }
-
-        Ext.Array.each(models, function(m) {
-            var summaryFields = TsSummaryRow.getFields();
-            _.forEach(summaryFields, function(summaryField) {
-                // Don't add fields that already exist
-                if (!m.getField(summaryField.name)) {
-                    //m.addField(summaryField);
-                }
-            });
-        });
-
-        _.each(Ext.Array.from(models), Rally.ui.grid.data.NodeInterface.decorate, Rally.ui.grid.data.NodeInterface);
-    }*/
 
     _getChildNodeFilters: function(node) {
         var parentType = node.self.typePath,
