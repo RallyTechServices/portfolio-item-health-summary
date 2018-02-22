d6ca4ee5eff677dde8a6968509b1d94d190845a3
Tyr Johanson <tyr@ca.com>
2018-02-22 16:53:14 +0000

    configurable team types for WIP

--- src/javascript/Constants.js
+++ src/javascript/Constants.js
@@ -4,21 +4,19 @@
 Ext.define("TsConstants", {
 
     statics: {
-        PORTFOLIO_ITEM_TYPE: 'PortfolioItem/Feature',
-        PORTFOLIO_ITEM_STORE_ID: 'PORTFOLIO_ITEM_STORE_ID',
-        GRID_STORE_ID: 'GRID_STORE_ID',
-        PER_PROJECT_WIP_LIMIT: 3,
-        PERIOD_LENGTH_SETTING: 'PERIOD_LENGTH_SETTING',
-        PERIOD_LENGTH_DEFAULT: 30,
-        MGMT_PROJECT_NAMES_SETTING: 'MGMT_PROJECT_NAMES_SETTING',
+        SETTINGS: {
+            PERIOD_LENGTH: 'PERIOD_LENGTH',
+            INCLUDED_PROJECT_TEAM_TYPES: 'INCLUDED_PROJECT_TEAM_TYPES',
+            PER_TEAM_WIP_MAX: 'PER_TEAM_WIP_MAX',
+        },
+
+        LABELS: {
+            WIP: 'Active Features Per-Dev Team (Average)',
+        },
+
         SELECTABLE_PORTFOLIO_ITEM_TYPE: 'PortfolioItem/Group',
         SELECTABLE_PORTFOLIO_ITEM_TYPE_LABEL: 'Affiliate',
-        ROW_PORTFOLIO_ITEM_TYPE: 'PortfolioItem/Epic',
         ROW_METRICS_PORTFOLIO_ITEM_TYPE: 'PortfolioItem/Feature',
-        WIP_LABEL: 'Active Features Per-Dev Team (Average)',
-
-        INCLUDED_PROJECT_TEAM_TYPES_SETTING: 'INCLUDED_PROJECT_TEAM_TYPES_SETTING',
-        PER_TEAM_WIP_MAX_SETTING: 'PER_TEAM_WIP_MAX_SETTING',
     }
 
 });
